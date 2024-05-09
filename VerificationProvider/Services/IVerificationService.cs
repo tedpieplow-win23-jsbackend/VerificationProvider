ec436488c1d0f11b5475607362ff894f553c0529
Kontiki94 <hassan.alheidari@hotmail.com>
2024-05-09 13:21:13 +0200

    Finished Verification code generator

---
+++ VerificationProvider/Services/IVerificationService.cs
@@ -0,0 +1,14 @@
+﻿using Azure.Messaging.ServiceBus;
+using VerificationProvider.Models;
+
+namespace VerificationProvider.Services
+{
+    public interface IVerificationService
+    {
+        string GenerateCode();
+        EmailRequest GenerateEmailRequest(VerificationRequest verificationRequest, string code);
+        string GenerateSBEmailRequest(EmailRequest emailRequest);
+        Task<bool> SaveVerificationRequest(VerificationRequest verificationRequest, string code);
+        VerificationRequest UnpackVerificationRequest(ServiceBusReceivedMessage message);
+    }
+}
